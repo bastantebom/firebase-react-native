c4a6be2c15159434573c33cff377cbd13e6b876c
hazelle-fullstack <hazelle@fullstack.ph>
2020-07-06 12:13:57 +0800

    onboarding text contents

---
+++ src/screens/onboarding.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const Onboarding = () => {
+  return (
+    <View>
+      <View>
+        <Text>Welcome to Servbees!</Text>
+        <Text>Find and offer goods, plus services, within your community. Pasabuy? Pabili? Easier on Servbees!</Text>
+      </View>
+      <View>
+        <Text>Discover and Buy</Text>
+        <Text>Looking for something in particular? Discover nearby options and get the best deals for goods and services.</Text>
+      </View>
+      <View>
+        <Text>Offer and Sell</Text>
+        <Text>Ready to be a Buzzybee? Offer your services and products to those near you. Find customers easily!</Text>
+      </View>
+      <View>
+        <Text>Join a Hive</Text>
+        <Text>Join our Hives to connect with people with the same interests and needs. Create your own HIves to organize your offers!</Text>
+      </View>
+    </View>
+  )
+}
+
+export default Onboarding
