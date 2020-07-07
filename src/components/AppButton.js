7d54817c65e65d7354a8c9f0a5cd57d2a6804716
Jayson Ilagan <jayson@360pixels.co>
2020-07-07 19:56:48 +0800

    apply colors and typography to button component

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -1,56 +1,50 @@
 import React from 'react';
-import {Text, View, TouchableOpacity, Dimensions} from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
+import AppText from './AppText/AppText';
+import AppColor from '../globals/Colors';
 
 const width = Dimensions.get('window').width;
 
-const AppButton = ({
-  text,
-  onPress,
-  type = 'filled',
-  size = 'large',
-  height = 'xs',
-}) => {
+const AppButton = ({text, onPress, type, size, height}) => {
   const large = width / 1.3;
-  const small = width / 2;
-  const btnSize = size === 'large' ? large : small;
-  const btnBgColor = type === 'filled' ? '#FFD400' : 'transparent';
-  const btnTextColor = '#2E3034';
+  const small = width / 2.5;
+  const btnSize = size === 'sm' ? small : large;
+  const btnBgColor =
+    type === 'primary' ? AppColor.primaryYellow : 'transparent';
+  const btnBorderColor =
+    type === 'primary' ? AppColor.primaryYellow : AppColor.contentEbony;
   const btnBorderRadius = 3;
   const btnHeight =
     height === 'xs'
       ? 2
-      : height === 's'
-      ? 3
-      : height === 'm'
+      : height === 'sm'
       ? 4
-      : height === 'l'
-      ? 5
-      : 6;
+      : height === 'md'
+      ? 8
+      : height === 'lg'
+      ? 10
+      : 14;
 
   const containerCommonStyle = {
     backgroundColor: btnBgColor,
     paddingVertical: btnHeight,
     width: btnSize,
-    borderRadius: btnBorderRadius,
-  };
-
-  const textCommonStyle = {
-    color: btnTextColor,
-    fontSize: 14,
-    textAlign: 'center',
-  };
-
-  const border = type === 'outlined' && {
-    borderColor: '#2E3034',
     borderWidth: 1.5,
+    borderColor: btnBorderColor,
+    borderRadius: btnBorderRadius,
+    justifyContent: 'center',
+    alignItems: 'center',
   };
 
   return (
     <TouchableOpacity onPress={onPress} activeOpacity={0.7}>
-      <View style={[containerCommonStyle, border]}>
-        <Text style={[textCommonStyle]}>
-          {text.charAt(0).toUpperCase() + text.slice(1)}
-        </Text>
+      <View style={[containerCommonStyle]}>
+        <AppText textStyle="button2">{text}</AppText>
       </View>
     </TouchableOpacity>
   );
