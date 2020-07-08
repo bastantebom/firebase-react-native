b1902c01deb6d0a3051b4fc855d925cf202f34e4
hazelle-fullstack <hazelle@fullstack.ph>
2020-07-08 13:37:20 +0800

    added custom style for AppText center aligned text

--- src/components/AppText/AppText.js
+++ src/components/AppText/AppText.js
@@ -2,14 +2,19 @@ import React, { Children } from 'react';
 import { Text, StyleSheet } from 'react-native';
 import styles from './AppText.scss'
 
-const AppText = ({ children, textStyle }) => {
+const AppText = ({ children, textStyle, customStyle }) => {
+  let computedTextStyle = styles[textStyle];
 
-    let computedTextStyle = styles[textStyle];
+  if (customStyle) {
+    computedTextStyle = {
+      ...computedTextStyle,
+      ...customStyle
+    }
+  }
 
-    return (
-        <Text style={computedTextStyle}>{children}</Text>
-    )
+  return (
+    <Text style={computedTextStyle}>{children}</Text>
+  )
 }
 
 export default AppText
-
--- src/screens/onboarding.js
+++ src/screens/onboarding.js
@@ -13,46 +13,48 @@ import Colors from '../globals/Colors';
 
 const styles = StyleSheet.create({
   slideHolder: {
-   flex: 1,
-   alignItems: 'center',
-   textAlign: 'center',
-   backgroundColor: Colors.primaryMidnightBlue, 
- },
- link: {
-  alignSelf: 'flex-end',
-  padding: 20
- },
- textHolder: {
-  padding: 15,
-  // textAlign: 'center'
- },
- dot: {
-  backgroundColor: Colors.neutralsWhite, 
-  width: 8, 
-  height: 8, 
-  borderRadius: 100, 
-  marginLeft: 3, 
-  marginRight: 3, 
-  marginTop: 3, 
-  marginBottom: 120
- },
- dotActive: {
-   backgroundColor: Colors.primaryMidnightBlue
- },
- image: {
-   position: 'absolute',
-   top: -100,
-   width: '100%',
- }
+    flex: 1,
+    alignItems: 'center',
+    textAlign: 'center',
+    backgroundColor: Colors.primaryMidnightBlue,
+  },
+  link: {
+    alignSelf: 'flex-end',
+    padding: 20
+  },
+  textHolder: {
+    padding: 15
+  },
+  dot: {
+    backgroundColor: Colors.neutralsWhite,
+    width: 8,
+    height: 8,
+    borderRadius: 100,
+    marginLeft: 3,
+    marginRight: 3,
+    marginTop: 3,
+    marginBottom: 120
+  },
+  dotActive: {
+    backgroundColor: Colors.primaryMidnightBlue
+  },
+  image: {
+    position: 'absolute',
+    top: -100,
+    width: '100%',
+  },
+  text: {
+    textAlign: 'center'
+  }
 })
 
 const Onboarding = () => {
   return (
     <Swiper
-      dot ={
+      dot={
         <View style={styles.dot} />
       }
-      activeDot ={
+      activeDot={
         <View style={[styles.dot, styles.dotActive]} />
       }
     >
@@ -62,9 +64,9 @@ const Onboarding = () => {
           <AppText textStyle="body2">Skip</AppText>
         </TouchableOpacity>
         <Image source={require('../images/onboarding-img1.png')} />
-        <AppText textStyle="display6">Welcome to Servbees!</AppText> 
+        <AppText textStyle="display6">Welcome to Servbees!</AppText>
         <View style={styles.textHolder}>
-          <AppText textStyle="body2">Find and offer goods, plus services, within your community. Pasabuy? Pabili? Easier on Servbees!</AppText>
+          <AppText textStyle="body2" customStyle={styles.text}>Find and offer goods, plus services, within your community. Pasabuy? Pabili? Easier on Servbees!</AppText>
         </View>
       </View>
       <View style={styles.slideHolder}>
@@ -73,9 +75,9 @@ const Onboarding = () => {
           <AppText textStyle="body2">Skip</AppText>
         </TouchableOpacity>
         <Image source={require('../images/onboarding-img2.png')} />
-        <AppText textStyle="display6">Discover and Buy</AppText> 
+        <AppText textStyle="display6" customStyle={styles.text}>Discover and Buy</AppText>
         <View style={styles.textHolder}>
-          <AppText textStyle="body2">Looking for something in particular? Discover nearby options and get the best deals for goods and services.</AppText>
+          <AppText textStyle="body2" customStyle={styles.text}>Looking for something in particular? Discover nearby options and get the best deals for goods and services.</AppText>
         </View>
       </View>
       <View style={styles.slideHolder}>
@@ -84,9 +86,9 @@ const Onboarding = () => {
           <AppText textStyle="body2">Skip</AppText>
         </TouchableOpacity>
         <Image source={require('../images/onboarding-img3.png')} />
-        <AppText textStyle="display6">Offer and Sell</AppText> 
+        <AppText textStyle="display6">Offer and Sell</AppText>
         <View style={styles.textHolder}>
-          <AppText textStyle="body2">Ready to be a Buzzybee? Offer your services and products to those near you. Find customers easily!</AppText>
+          <AppText textStyle="body2" customStyle={styles.text}>Ready to be a Buzzybee? Offer your services and products to those near you. Find customers easily!</AppText>
         </View>
       </View>
       <View style={styles.slideHolder}>
@@ -95,9 +97,9 @@ const Onboarding = () => {
           <AppText textStyle="body2">Skip</AppText>
         </TouchableOpacity>
         <Image source={require('../images/onboarding-img4.png')} />
-        <AppText textStyle="display6">Join a Hive</AppText> 
+        <AppText textStyle="display6">Join a Hive</AppText>
         <View style={styles.textHolder}>
-          <AppText textStyle="body2">Join our Hives to connect with people with the same interests and needs. Create your own Hives to organize your offers!</AppText>
+          <AppText textStyle="body2" customStyle={styles.text}>Join our Hives to connect with people with the same interests and needs. Create your own Hives to organize your offers!</AppText>
         </View>
       </View>
     </Swiper>
@@ -105,5 +107,3 @@ const Onboarding = () => {
 }
 
 export default Onboarding
-
-
