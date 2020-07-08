6eb5a54d278a183cde0b2f3a18c44aadce9dfe6e
hazelle-fullstack <hazelle@fullstack.ph>
2020-07-08 13:37:20 +0800

    added custom style for AppText center aligned text

--- src/components/AppText/AppText.js
+++ src/components/AppText/AppText.js
@@ -3,9 +3,19 @@ import { Text } from 'react-native';
 import styles from './AppText.scss'
 
 const AppText = ({ children, textStyle, customStyle }) => {
+<<<<<<< HEAD
+=======
+  let computedTextStyle = styles[textStyle];
+>>>>>>> added custom style for AppText center aligned text
 
-    let computedTextStyle = styles[textStyle];
+  if (customStyle) {
+    computedTextStyle = {
+      ...computedTextStyle,
+      ...customStyle
+    }
+  }
 
+<<<<<<< HEAD
     if (customStyle) {
         computedTextStyle = {
             ...computedTextStyle,
@@ -16,7 +26,11 @@ const AppText = ({ children, textStyle, customStyle }) => {
     return (
         <Text style={computedTextStyle}>{children}</Text>
     )
+=======
+  return (
+    <Text style={computedTextStyle}>{children}</Text>
+  )
+>>>>>>> added custom style for AppText center aligned text
 }
 
 export default AppText
-
