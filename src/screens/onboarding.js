842482390504749e3ff90f4cb13f4ed7750298bc
geraldine <geraldine@fullstack.ph>
2020-07-10 18:28:28 +0800

    update styling on login component

--- src/screens/onboarding.js
+++ src/screens/onboarding.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
   btnHolder: {
     position: 'absolute',
     top: height - 130,
-    zIndex: 2,
+    // zIndex: 2,
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
