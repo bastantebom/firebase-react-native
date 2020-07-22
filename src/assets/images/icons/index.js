b06822ed073222ca98d427bd1a8ed7c11a1d0319
Paul Prieto <paul@360pixels.co>
2020-07-22 14:55:25 +0800

    chore: export icons for dashboard

--- src/assets/images/icons/index.js
+++ src/assets/images/icons/index.js
@@ -5,23 +5,63 @@
  * <Close />
  */
 
+//  notification
 import CircleTick from '@/assets/images/icons/circle-tick.svg';
 import Warning from '@/assets/images/icons/warning.svg';
 
+// utils
 import Close from '@/assets/images/icons/close.svg';
 import CloseDark from '@/assets/images/icons/close-dark.svg';
 import CloseLight from '@/assets/images/icons/close-light.svg';
 
+// password
 import EyeDark from '@/assets/images/icons/eye-dark.svg';
 import EyeLight from '@/assets/images/icons/eye-light.svg';
 
+// 3rd party
 import Facebook from '@/assets/images/icons/facebook.svg';
 import Google from '@/assets/images/icons/google.svg';
 
+// navigation/maps
 import NavigationArrow from '@/assets/images/icons/navigation-arrow.svg';
 import NavigationPin from '@/assets/images/icons/navigation-pin.svg';
 
+// Dashboard
+import Filter from '@/assets/images/icons/filter.svg';
+import JarHeart from '@/assets/images/icons/jar-heart.svg';
+
+
+// For bottom navigation
+import NotificationDot from '@/assets/images/icons/notification-red-dot.svg';
+import PostBG from '@/assets/images/icons/post-bg.svg';
+import PostPlus from '@/assets/images/icons/post-plus.svg';
+
+import Bell from '@/assets/images/icons/bell.svg';
+import BellActive from '@/assets/images/icons/bell-active.svg';
+
+import Hive from '@/assets/images/icons/hive.svg';
+import HiveActive from '@/assets/images/icons/hive-active.svg';
+
+import ServbeesAlt from '@/assets/images/icons/servbees-alt.svg';
+import ServbeesAltActive from '@/assets/images/icons/servbees-alt-active.svg';
+
+import UserAlt from '@/assets/images/icons/user-alt.svg';
+import UserAltActive from '@/assets/images/icons/user-alt-active.svg';
+
 export {
+    Filter,
+    JarHeart,
+    NotificationDot,
+    PostBG,
+    PostPlus,
+    Bell,
+    BellActive,
+    Hive,
+    HiveActive,
+    ServbeesAlt,
+    ServbeesAltActive,
+    UserAlt,
+    UserAltActive,
     CircleTick,
     Close, 
     CloseDark, 
