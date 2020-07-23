dee209576d54e249b67ed4011f1d0f3e972ccc4a
geraldine-fullstack <geraldine@fullstack.ph>
2020-07-23 13:25:20 +0800

    apply hexagon border to image upload

--- src/components/ImageUpload/HexagonBorder.js
+++ src/components/ImageUpload/HexagonBorder.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import AppText from '@/components/AppText/AppText'
 import ImagePicker from 'react-native-image-crop-picker';
 import AppButton from '@/components/AppButton';
-import { View, Image } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import {
   Svg,
   Circle,
@@ -10,37 +10,36 @@ import {
   Rect,
   Defs,
   ClipPath,
-  Text,
-  // Image
+  Image,
+  SvgUri
 } from 'react-native-svg';
 
-const HexagonBorder = () => {
-
-  const [imageSource, setImageSource] = useState(null);
+const HexagonBorder = ({ imgSrc }) => {
 
   return (
-    <View>
-      <Svg height="100% " width="100%" viewBox="0 0 100 100">
+    <View >
+      <Svg height="500" width="500" viewBox="0 0 100 100">
         <Defs>
           <ClipPath id="clip">
             <Polygon
               points="71.65063509461098,62.5 50,75 28.349364905389038,62.50000000000001 28.34936490538903,37.50000000000001 49.99999999999999,25 71.65063509461098,37.50000000000001"
-              cx="50%" 
-              cy="50%"
+              // cx="100%" 
+              // cy="100%"
             />
           </ClipPath>
         </Defs>
         <Image
-          x="15%"
-          y="15%"
+          // x="0"
+          // y="0"
           width="100%"
           height="100%"
           // preserveAspectRatio="xMidYMid slice"
-          opacity={1}
-          // href={require("../../assets/images/image-1.png")}
+          opacity="1"
+          href={imgSrc}
           clipPath="url(#clip)"
         />
       </Svg>
+      <Text>hi</Text>
     </View>
   )
 }
