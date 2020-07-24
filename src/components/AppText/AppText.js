1b6ce8098e943dfee582dea3b752fe76c51514ad
Paul Prieto <paul@360pixels.co>
2020-07-24 11:56:35 +0800

    refactor: Add color prop to AppText

--- src/components/AppText/AppText.js
+++ src/components/AppText/AppText.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { Text, Dimensions, Platform } from 'react-native';
 import styles from './AppText.scss';
+import { Colors } from '@/globals'
 
 /**
  * 
  * @param {listed on Apptext.scss} textStyle
  */
 
-const AppText = ({ children, textStyle, customStyle }) => {
+const AppText = ({ children, textStyle, customStyle, color }) => {
   let computedTextStyle = styles[textStyle];
 
   if (customStyle) {
     computedTextStyle = {
       ...computedTextStyle,
       ...customStyle,
+      color: color ? color : Colors.contentEbony
     };
   }
 
-  if (Platform.OS === 'android') { 
-    console.log(Dimensions.get('window').height) 
+  if (Platform.OS === 'android') {
+    console.log(Dimensions.get('window').height)
     console.log(Dimensions.get('window').width)
   }
 
