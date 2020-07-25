3df07a9a489a217e35aca3688a6a95aea998242f
Paul Prieto <paul@360pixels.co>
2020-07-25 16:08:09 +0800

    chore: add index file for authentication screens

---
+++ src/screens/Authentication/index.js
@@ -0,0 +1,6 @@
+import Login from './Login/login';
+import ResetPassword from './ResetPassword';
+import SignUp from './SignUp/SignUp';
+import VerifyAccount from './SignUp/VerifyAccount';
+
+export {Login, ResetPassword, SignUp, VerifyAccount};
