4742b133c619ecb34f2fdba04545f298540ac326
Paul Prieto <paul@360pixels.co>
2020-07-25 14:40:55 +0800

    chore: create index for svg images and illustration.

---
+++ src/assets/images/index.js
@@ -0,0 +1,25 @@
+// for SVG images only
+
+// big icons
+import Location from './location.svg';
+import ResetPassword from './reset-password.svg';
+import Verify from './verify.svg';
+
+// illustrations
+import OnboardingIllustration1 from './onboarding-img1.svg';
+import OnboardingIllustration2 from './onboarding-img2.svg';
+import OnboardingIllustration3 from './onboarding-img3.svg';
+import OnboardingIllustration4 from './onboarding-img4.svg';
+
+import PolygonStatic from './polygon-static.svg';
+
+export {
+  Location,
+  ResetPassword,
+  Verify,
+  OnboardingIllustration1,
+  OnboardingIllustration2,
+  OnboardingIllustration3,
+  OnboardingIllustration4,
+  PolygonStatic,
+};
