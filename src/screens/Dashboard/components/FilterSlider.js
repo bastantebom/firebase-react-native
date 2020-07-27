882b785806790966ac3f670b7405db9031a7d8e4
Paul Prieto <paul@360pixels.co>
2020-07-27 16:02:04 +0800

    chore: create apply and reset button for filter screen

--- src/screens/Dashboard/components/FilterSlider.js
+++ src/screens/Dashboard/components/FilterSlider.js
@@ -1,5 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import {View, TouchableOpacity, Dimensions, StyleSheet} from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
 
 import {AppText, AppViewContainer, AppRadio, AppCheckbox} from '@/components';
 import {normalize, Colors} from '@/globals';
@@ -105,143 +111,154 @@ const FilterSlider = () => {
         backgroundColor: 'white',
         height: Dimensions.get('window').height,
       }}>
-      <AppViewContainer paddingSize={3}>
-        <View style={{flexDirection: 'row', marginBottom: 40}}>
-          <View style={{flex: 1}}>
-            <AppText textStyle="subtitle1">Filter & Sort</AppText>
-            <AppText textStyle="caption" color={Colors.contentPlaceholder}>
-              Select all that applies
-            </AppText>
+      <ScrollView style={{ paddingTop: 24}}>
+        <AppViewContainer marginSize={3}>
+          <View style={{flexDirection: 'row', marginBottom: 40}}>
+            <View style={{flex: 1}}>
+              <AppText textStyle="subtitle1">Filter & Sort</AppText>
+              <AppText textStyle="caption" color={Colors.contentPlaceholder}>
+                Select all that applies
+              </AppText>
+            </View>
+
+            <TouchableOpacity
+              onPress={() => setModalState(false)}
+              style={{justifyContent: 'flex-start'}}>
+              <CloseDark width={normalize(24)} height={normalize(24)} />
+            </TouchableOpacity>
           </View>
 
-          <TouchableOpacity
-            onPress={() => setModalState(false)}
-            style={{justifyContent: 'flex-start'}}>
-            <CloseDark width={normalize(24)} height={normalize(24)} />
+          <View style={{marginBottom: 32}}>
+            <AppText textStyle="subtitle2" customStyle={{marginBottom: 16}}>
+              Filter by
+            </AppText>
+            <AppCheckbox
+              Icon={() => {
+                return <FilterServices />;
+              }}
+              label="Services"
+              value={checkboxServices}
+              valueChangeHandler={checkboxServicesHandler}
+              style={{marginBottom: 16}}
+            />
+            <AppCheckbox
+              Icon={() => {
+                return <FilterSeller />;
+              }}
+              label="Seller"
+              value={checkboxSeller}
+              valueChangeHandler={checkboxSellerHandler}
+              style={{marginBottom: 16}}
+            />
+            <AppCheckbox
+              Icon={() => {
+                return <FilterNeeds />;
+              }}
+              label="Needs"
+              value={checkboxNeeds}
+              valueChangeHandler={checkboxNeedsHandler}
+              style={{marginBottom: 16}}
+            />
+          </View>
+
+          <View>
+            <AppText textStyle="subtitle2" customStyle={{marginBottom: 16}}>
+              Sort by
+            </AppText>
+            <AppRadio
+              Icon={() => {
+                return <FilterNeeds />;
+              }}
+              label="Popular"
+              name="Popular"
+              value={radioButtons.Popular}
+              valueChangeHandler={radioHandler}
+              style={{marginBottom: 16}}
+            />
+            <AppRadio
+              Icon={() => {
+                return <FilterNeeds />;
+              }}
+              label="Recent"
+              name="Recent"
+              value={radioButtons.Recent}
+              valueChangeHandler={radioHandler}
+              style={{marginBottom: 16}}
+            />
+            <AppRadio
+              Icon={() => {
+                return <FilterNeeds />;
+              }}
+              label="Nearest"
+              name="Nearest"
+              value={radioButtons.Nearest}
+              valueChangeHandler={radioHandler}
+              style={{marginBottom: 16}}
+            />
+            <AppRadio
+              Icon={() => {
+                return <FilterNeeds />;
+              }}
+              label="Price High to Low"
+              name="HighLow"
+              value={radioButtons.HighLow}
+              valueChangeHandler={radioHandler}
+              style={{marginBottom: 16}}
+            />
+            <AppRadio
+              Icon={() => {
+                return <FilterNeeds />;
+              }}
+              label="Price Low to High"
+              name="LowHigh"
+              value={radioButtons.LowHigh}
+              valueChangeHandler={radioHandler}
+              style={{marginBottom: 16}}
+            />
+          </View>
+        </AppViewContainer>
+
+        <View
+          style={{
+            marginBottom: 40,
+            borderTopWidth: 1,
+            borderTopColor: Colors.neutralsZircon,
+            paddingTop: 24,
+            paddingHorizontal: 24,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}>
+          <TouchableOpacity>
+            <View style={styles.buttonWhite}>
+              <AppText textStyle="button2">Reset</AppText>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <View style={styles.buttonYellow}>
+              <AppText textStyle="button2">Apply</AppText>
+            </View>
           </TouchableOpacity>
         </View>
-
-        <View style={{marginBottom: 32}}>
-          <AppText textStyle="subtitle2" customStyle={{marginBottom: 16}}>
-            Filter by
-          </AppText>
-          <AppCheckbox
-            Icon={() => {
-              return <FilterServices />;
-            }}
-            label="Services"
-            value={checkboxServices}
-            valueChangeHandler={checkboxServicesHandler}
-            style={{marginBottom: 16}}
-          />
-          <AppCheckbox
-            Icon={() => {
-              return <FilterSeller />;
-            }}
-            label="Seller"
-            value={checkboxSeller}
-            valueChangeHandler={checkboxSellerHandler}
-            style={{marginBottom: 16}}
-          />
-          <AppCheckbox
-            Icon={() => {
-              return <FilterNeeds />;
-            }}
-            label="Needs"
-            value={checkboxNeeds}
-            valueChangeHandler={checkboxNeedsHandler}
-            style={{marginBottom: 16}}
-          />
-        </View>
-
-        <View>
-          <AppText textStyle="subtitle2" customStyle={{marginBottom: 16}}>
-            Sort by
-          </AppText>
-          <AppRadio
-            Icon={() => {
-              return <FilterNeeds />;
-            }}
-            label="Popular"
-            name="Popular"
-            value={radioButtons.Popular}
-            valueChangeHandler={radioHandler}
-            style={{marginBottom: 16}}
-          />
-          <AppRadio
-            Icon={() => {
-              return <FilterNeeds />;
-            }}
-            label="Recent"
-            name="Recent"
-            value={radioButtons.Recent}
-            valueChangeHandler={radioHandler}
-            style={{marginBottom: 16}}
-          />
-          <AppRadio
-            Icon={() => {
-              return <FilterNeeds />;
-            }}
-            label="Nearest"
-            name="Nearest"
-            value={radioButtons.Nearest}
-            valueChangeHandler={radioHandler}
-            style={{marginBottom: 16}}
-          />
-          <AppRadio
-            Icon={() => {
-              return <FilterNeeds />;
-            }}
-            label="Price High to Low"
-            name="HighLow"
-            value={radioButtons.HighLow}
-            valueChangeHandler={radioHandler}
-            style={{marginBottom: 16}}
-          />
-          <AppRadio
-            Icon={() => {
-              return <FilterNeeds />;
-            }}
-            label="Price Low to High"
-            name="LowHigh"
-            value={radioButtons.LowHigh}
-            valueChangeHandler={radioHandler}
-            style={{marginBottom: 16}}
-          />
-        </View>
-      </AppViewContainer>
-
-      <View
-        style={{
-          marginBottom: 40,
-          borderTopWidth: 1,
-          borderTopColor: Colors.neutralsZircon,
-          paddingTop: 24,
-          paddingHorizontal: 24,
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-        }}>
-        <TouchableOpacity>
-          <View style={styles.buttonContainer}>
-            <AppText>Reset</AppText>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <View style={styles.buttonContainer}>
-            <AppText>Apply</AppText>
-          </View>
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
+  buttonWhite: {
     paddingHorizontal: 40,
     paddingVertical: 8,
     borderColor: Colors.contentEbony,
-    borderWidth: 1,
+    borderWidth: 1.5,
+    borderRadius: 4
+  },
+  buttonYellow: {
+    paddingHorizontal: 40,
+    paddingVertical: 8,
+    borderColor: Colors.primaryYellow,
+    backgroundColor: Colors.primaryYellow,
+    borderWidth: 1.5,
+    borderRadius: 4
   },
 });
 
