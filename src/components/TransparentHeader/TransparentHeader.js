2e1e094fb4a78fdbbc3af80930c9d4381139b215
Paul Prieto <paul@360pixels.co>
2020-07-28 10:48:27 +0800

    chore: register transparent header on components index

---
+++ src/components/TransparentHeader/TransparentHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+import {AppText} from '@/components';
+
+const TransparentHeader = () => {
+    return (
+        <View>
+            <AppText>Hello Header</AppText>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default TransparentHeader;
