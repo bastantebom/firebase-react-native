a49b04238eb4ff297a404ba586ea896f1b72b7a8
Paul Prieto <paul@360pixels.co>
2020-07-28 17:15:34 +0800

    chore: add button to change state of header for demo purpose.

--- src/screens/Profile/profile.js
+++ src/screens/Profile/profile.js
@@ -44,6 +44,12 @@ function Profile({navigation}) {
   const [following, setFollowing] = useState(false);
   const [menu, setMenu] = useState(false);
 
+  const [headerState, setHeaderState] = useState('own');
+
+  const changeHeaderHandler = () => {
+    headerState === 'own' ? setHeaderState('other') : setHeaderState('own');
+  };
+
   const toggleEllipsisState = () => {
     setEllipsisState(!ellipsisState);
   };
@@ -85,7 +91,7 @@ function Profile({navigation}) {
   return (
     <>
       <TransparentHeader
-        type="own"
+        type={headerState}
         ellipsisState={ellipsisState}
         toggleEllipsisState={toggleEllipsisState}
         toggleFollowing={toggleFollowing}
@@ -113,6 +119,7 @@ function Profile({navigation}) {
           <ProfileImageUpload />
           <HexagonBorder />
 
+          <Button title="Change header" onPress={changeHeaderHandler} />
           <Button title="sign out" onPress={signOut} />
           <Button title="show bottom modal" onPress={toggleEllipsisState} />
         </View>
