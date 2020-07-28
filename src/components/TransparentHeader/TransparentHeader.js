ef30074f07dae3692f7e0f9ffc31855d9e7b1665
Paul Prieto <paul@360pixels.co>
2020-07-28 14:43:04 +0800

    chore: create bottom modal on click of ellipsis

--- src/components/TransparentHeader/TransparentHeader.js
+++ src/components/TransparentHeader/TransparentHeader.js
@@ -8,11 +8,16 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 import Modal from 'react-native-modal';
+<<<<<<< HEAD
 
 import {AppText, BottomSheetHeader} from '@/components';
 import EllipsisMenu from './components/EllipsisMenu';
 import OwnMenu from './components/OwnMenu';
 
+=======
+
+import {AppText, BottomSheetHeader} from '@/components';
+>>>>>>> chore: create bottom modal on click of ellipsis
 import {
   HeaderBack,
   HeaderShare,
@@ -27,6 +32,7 @@ import {
 } from '@/assets/images/icons';
 import {normalize, GlobalStyle} from '@/globals';
 
+<<<<<<< HEAD
 const TransparentHeader = ({
   toggleEllipsisState,
   ellipsisState,
@@ -195,6 +201,100 @@ const TransparentHeader = ({
         </Modal>
       </>
     );
+=======
+const TransparentHeader = ({toggleEllipsisState, ellipsisState}) => {
+  return (
+    <>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          position: 'absolute',
+          zIndex: 1,
+          backgroundColor: 'transparent',
+        }}>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            paddingHorizontal: 16,
+            width: Dimensions.get('window').width,
+            paddingTop: 4,
+          }}>
+          {/* Left aligned icons */}
+          <View>
+            <TouchableOpacity activeOpacity={0.7}>
+              <View style={styles.circle}>
+                <HeaderBack width={normalize(16)} height={normalize(16)} />
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          {/* Right aligned icons */}
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity activeOpacity={0.7}>
+              <View style={[styles.followButton, GlobalStyle.marginLeft1]}>
+                <HeaderFollow width={normalize(16)} height={normalize(16)} />
+                <AppText
+                  textStyle="button3"
+                  color="white"
+                  customStyle={{marginLeft: 4}}>
+                  Follow
+                </AppText>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity activeOpacity={0.7}>
+              <View style={[styles.circle, GlobalStyle.marginLeft1]}>
+                <HeaderShare width={normalize(16)} height={normalize(16)} />
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity activeOpacity={0.7} onPress={toggleEllipsisState}>
+              <View style={[styles.circle, GlobalStyle.marginLeft1]}>
+                <HeaderEllipsis width={normalize(16)} height={normalize(16)} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+
+      <Modal
+        isVisible={ellipsisState}
+        animationIn="slideInUp"
+        animationInTiming={500}
+        animationOut="slideOutDown"
+        animationOutTiming={500}
+        onSwipeComplete={toggleEllipsisState}
+        swipeDirection="down"
+        style={{
+          justifyContent: 'flex-end',
+          margin: 0,
+        }}
+        customBackdrop={
+          <TouchableWithoutFeedback
+            onPress={() => {
+              toggleEllipsisState();
+            }}>
+            <View style={{flex: 1, backgroundColor: 'black'}} />
+          </TouchableWithoutFeedback>
+        }>
+        {/* <FilterSlider modalToggler={toggleModal} /> */}
+        <View
+          style={{
+            backgroundColor: 'white',
+            paddingBottom: 32,
+            borderTopEndRadius: 8,
+            borderTopStartRadius: 8,
+          }}>
+          <BottomSheetHeader />
+          <AppText>Hello</AppText>
+          <AppText>Hello</AppText>
+          <AppText>Hello</AppText>
+        </View>
+      </Modal>
+    </>
+  );
+>>>>>>> chore: create bottom modal on click of ellipsis
 };
 
 const styles = StyleSheet.create({
