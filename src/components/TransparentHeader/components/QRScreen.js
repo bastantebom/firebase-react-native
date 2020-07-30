c6d19a6203b1ec196c349dc86117e6af1d231e61
Paul Christian Prieto <pcmprieto@gmail.com>
2020-07-30 17:29:50 +0800

    chore: create tab page for QR screem

--- src/components/TransparentHeader/components/QRScreen.js
+++ src/components/TransparentHeader/components/QRScreen.js
@@ -8,69 +8,105 @@ import {
   StyleSheet,
 } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
+import Dash from 'react-native-dash';
+import Share from "react-native-share";
 
 import { AppText, PaddingView, TabNavigation } from '@/components';
 import { Colors, normalize } from '@/globals';
 
-import { HeaderBackGray } from '@/assets/images/icons';
+import { HeaderBackGray, QRLink, QRDownload, QRShare } from '@/assets/images/icons';
 
 const QRTab = () => {
   let logoFromFile = require('@/assets/images/logo.png');
 
+  const copyHandler = () => {
+    console.log("copy to clipboard")
+  }
+
+  const shareHandler = async () => {
+    const shareOptions = {
+      title: 'Share profile',
+      url: 'servbees.com/waynesburger',
+      failOnCancel: false,
+    };
+
+    try {
+      const ShareResponse = await Share.open(shareOptions);
+      setResult(JSON.stringify(ShareResponse, null, 2));
+    } catch (error) {
+      console.log('Error =>', error);
+      setResult('error: '.concat(getErrorString(error)));
+    }
+  };
+
   return (
-    <View
-      style={{
-        elevation: 9,
-        padding: 5,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 12,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 0 },
-        shadowOpacity: 0.4,
-        padding: normalize(66),
-        shadowRadius: 12,
-        backgroundColor: 'white',
-        position: 'relative',
-      }}>
-      <QRCode
-        value="http://awesome.lissnk.qr"
-        logo={logoFromFile}
-        logoSize={normalize(60)}
-        size={normalize(187)}
-        color={'#1F1A54'}
-      />
-      <View>
-        <AppText textStyle="display6">Wayne Jansen Tayco</AppText>
-      </View>
+    <>
+      <PaddingView paddingSize={4} style={{ paddingBottom: 16 }}>
+        <View
+          style={{
+            elevation: 6,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 12,
+            shadowColor: 'rgba(65, 65, 65, 0.2)',
+            shadowOffset: { width: 0, height: 40 },
+            shadowRadius: 12,
+            backgroundColor: 'white',
+            position: 'relative',
+          }}>
+          <View style={{ padding: normalize(65), paddingBottom: 0 }}>
+            <QRCode
+              value="http://awesome.lissnk.qr"
+              logo={logoFromFile}
+              logoSize={normalize(60)}
+              size={normalize(187)}
+              color={'#1F1A54'}
 
-      <View
-        style={{
-          width: 50,
-          height: 50,
-          borderRadius: 25,
-          backgroundColor: 'white',
-          position: 'absolute',
-          left: -25,
-          bottom: -25,
-          zIndex: 5,
-        }}
-      />
+            />
+          </View>
 
-      <View
-        style={{
-          width: 50,
-          height: 50,
-          borderRadius: 25,
-          backgroundColor: 'white',
-          position: 'absolute',
-          right: -25,
-          bottom: -25,
-          zIndex: 5,
-        }}
-      />
-    </View>
+          <View style={{ marginTop: 16, }}>
+            <AppText textStyle="display6" customStyle={{ textAlign: 'center' }}>Wayne Jansen Tayco</AppText>
+          </View>
+
+          <Dash style={{ width: '100%', height: 1, marginVertical: 24, }} dashLength={8} dashGap={4} dashColor={Colors.neutralGray} dashThickness={1.7} />
+
+          <View style={{ flex: 1, width: '100%' }} >
+            <View style={{ flexDirection: 'row', backgroundColor: Colors.secondarySolitude, maxWidth: normalize(260), alignSelf: 'center', alignItems: "center" }}>
+
+              <AppText customStyle={{ paddingVertical: 10, paddingHorizontal: 8, width: normalize(220) }} textStyle="caption" >servbees.com/waynesburger</AppText>
+              <TouchableOpacity activeOpacity={.7} onPress={copyHandler}>
+                <View style={{ backgroundColor: Colors.primaryYellow, height: normalize(40), width: normalize(40), justifyContent: 'center', alignItems: 'center' }}>
+                  <QRLink width={normalize(24)} height={normalize(24)} />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <View style={{ flexDirection: 'row', marginTop: 16, marginBottom: 24 }}>
+            <TouchableOpacity activeOpacity={.7}>
+              <View style={{ flexDirection: "row", alignItems: 'center' }}>
+                <QRDownload width={normalize(24)} height={normalize(24)} />
+                <AppText textStyle="body1" color={Colors.contentPlaceholder} customStyle={{ marginLeft: 4 }}>Save to device</AppText>
+              </View>
+            </TouchableOpacity>
+
+
+            <TouchableOpacity activeOpacity={.7} onPress={shareHandler}>
+              <View style={{ flexDirection: "row", alignItems: 'center', marginLeft: 24 }}>
+                <QRShare width={normalize(24)} height={normalize(24)} />
+                <AppText textStyle="body1" color={Colors.contentPlaceholder} customStyle={{ marginLeft: 4 }}>Share</AppText>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+      </PaddingView>
+
+      <AppText customStyle={{ textAlign: 'center', paddingHorizontal: normalize(66) }} textStyle="body2">
+        Your friends can scan this code to go to your Servbees profile.
+      </AppText>
+    </>
   )
 }
 
