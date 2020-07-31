c510370d48e32f9ff053e3bcf4578756503a8c1f
Paul Christian Prieto <pcmprieto@gmail.com>
2020-08-01 00:51:38 +0800

    fix: position of card when active

--- src/screens/Post/post.js
+++ src/screens/Post/post.js
@@ -16,47 +16,108 @@ const Post = () => {
   const [needPosition] = useState(new Animated.Value(0))
   const [needSize] = useState(new Animated.Value(normalize(114)))
 
+  const [containerPadding] = useState(new Animated.Value(0))
+
 
   const selectActive = (selectedCard) => {
     console.log("make me big")
     setPickingState(!pickingState)
 
     if (pickingState) {
-
       switch (selectedCard) {
         case 'post':
           Animated.parallel([
             Animated.timing(postSize, {
-              toValue: normalize(250),
+              toValue: normalize(271),
               duration: 500,
               useNativeDriver: false
             }),
-            // Animated.timing(sellPosition, {
-            //   toValue: normalize(78),
-            //   duration: 500,
-            //   useNativeDriver: false
-            // }),
-            // Animated.timing(needPosition, {
-            //   toValue: normalize(78),
-            //   duration: 500,
-            //   useNativeDriver: false
-            // }),
-
+            Animated.timing(needSize, {
+              toValue: normalize(114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(needPosition, {
+              toValue: normalize(-74),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellSize, {
+              toValue: normalize(114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellPosition, {
+              toValue: normalize(15),
+              duration: 500,
+              useNativeDriver: false
+            }),
           ]).start()
           break;
         case 'need':
           Animated.parallel([
             Animated.timing(needSize, {
-              toValue: normalize(250),
+              toValue: normalize(271),
               duration: 500,
               useNativeDriver: false
             }),
+            Animated.timing(postSize, {
+              toValue: normalize(114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellSize, {
+              toValue: normalize(114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(postPosition, {
+              toValue: normalize(114 + 114 + 74 - 8),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellPosition, {
+              toValue: normalize(114 + 74 + 9 + 9),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(needPosition, {
+              toValue: normalize(-(114+ 114+ 9 + 9)),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            
           ]).start()
           break;
         case 'sell':
           Animated.parallel([
             Animated.timing(sellSize, {
-              toValue: normalize(250),
+              toValue: normalize(271),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(needSize, {
+              toValue: normalize(114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(postSize, {
+              toValue: normalize(114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(postPosition, {
+              toValue: normalize(114 + 114 + 74 - 8),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellPosition, {
+              toValue: normalize(-9 -114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(needPosition, {
+              toValue: normalize(-74),
               duration: 500,
               useNativeDriver: false
             }),
@@ -76,6 +137,21 @@ const Post = () => {
               duration: 500,
               useNativeDriver: false
             }),
+            Animated.timing(needPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(postPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
           ]).start()
           break;
 
@@ -83,6 +159,21 @@ const Post = () => {
           Animated.parallel([
             Animated.timing(needSize, {
               toValue: normalize(114),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(needPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(postPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellPosition, {
+              toValue: normalize(0),
               duration: 500,
               useNativeDriver: false
             }),
@@ -96,6 +187,21 @@ const Post = () => {
               duration: 500,
               useNativeDriver: false
             }),
+            Animated.timing(needPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(postPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
+            Animated.timing(sellPosition, {
+              toValue: normalize(0),
+              duration: 500,
+              useNativeDriver: false
+            }),
           ]).start()
           break;
       }
@@ -106,21 +212,30 @@ const Post = () => {
 
   let PostAnimationStyle = {
     transform: [{ translateX: postPosition }],
-    width: postSize
+    width: postSize,
   }
 
   let SellAnimationStyle = {
     transform: [{ translateX: sellPosition }],
-    width: sellSize
+    width: sellSize,
   }
 
   let NeedAnimationStyle = {
     transform: [{ translateX: needPosition }],
-    width: needSize
+    width: needSize,
   }
 
+
+
   return (
-    <View style={styles.postAnimationContainer} >
+    <View style={[
+      styles.postAnimationContainer,
+      {
+        // justifyContent: pickingState ? 'flex-start' : 'space-around',
+        // position: pickingState ? 'relative' : 'absolute',
+        paddingLeft: normalize(8),
+      }
+    ]}>
       <Animated.View style={[styles.postCard, PostAnimationStyle]}>
         <TouchableOpacity onPress={() => selectActive('post')}>
           <View style={{ height: '100%' }}>
@@ -152,22 +267,26 @@ const styles = StyleSheet.create({
   postAnimationContainer: {
     backgroundColor: 'gray',
     flexDirection: 'row',
-    position: 'relative'
+    position: 'relative',
+    height: normalize(88)
   },
   sellCard: {
     backgroundColor: 'blue',
-    height: normalize(100),
-    borderRadius: 20
+    height: normalize(70),
+    borderRadius: 20,
+    marginLeft: normalize(9)
   },
   postCard: {
     backgroundColor: 'pink',
-    height: normalize(100),
-    borderRadius: 20
+    height: normalize(70),
+    borderRadius: 20,
+    // marginLeft: normalize(8)
   },
   needCard: {
     backgroundColor: 'green',
-    height: normalize(100),
-    borderRadius: 20
+    height: normalize(70),
+    borderRadius: 20,
+    marginLeft: normalize(9)
   }
 });
 
