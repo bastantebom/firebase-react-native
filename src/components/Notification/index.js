6d3a892753a28c0d755de83c74a1ff862a0d8c66
Paul Christian Prieto <pcmprieto@gmail.com>
2020-08-03 14:55:15 +0800

    chore: add get verified badge notification

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,54 +1,78 @@
-import React, { useContext } from 'react';
-import { StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+import React, {useContext} from 'react';
+import {StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 
 import PaddingView from '@/components/AppViewContainer/PaddingView';
 
 // import CloseDark from '@/assets/images/icons/close-dark.svg';
 // import CircleTick from '@/assets/images/icons/circle-tick.svg';
 
-import { CloseDark, CloseLight, CircleTick, Warning,  } from '@/assets/images/icons';
-import Colors from '@/globals/Colors'
+import {
+  CloseDark,
+  CloseLight,
+  CircleTick,
+  Warning,
+  Verified,
+} from '@/assets/images/icons';
+import Colors from '@/globals/Colors';
 
-import { Context } from '@/context';
+import {Context} from '@/context';
+import {normalize} from '@/globals';
 
 /**
- * 
+ *
  * @param {
- *     message: <Text> string </Text> 
+ *     message: <Text> string </Text>
  *     type: "error" | "success"
- * } 
+ * }
  */
 
-const Notification = ({ message, type }) => {
-    const { notificationState, closeNotification } = useContext(Context);
+const Notification = ({message, type, position = 'absolute'}) => {
+  const {notificationState, closeNotification} = useContext(Context);
 
-    const width = Dimensions.get('window').width;
+  const width = Dimensions.get('window').width;
 
-    const styles = StyleSheet.create({
-        container: {
-            width: width,
-            position: "absolute",
-            top: 0,
-            left: 0,
-            flexDirection: 'row',
-            zIndex: 1,
-            backgroundColor: type === 'success' ? Colors.yellow2 : Colors.secondaryBrinkPink
-        }
-    })
+  const styles = StyleSheet.create({
+    container: {
+      width: width,
+      position: position,
+      top: 0,
+      left: 0,
+      flexDirection: 'row',
+      zIndex: 1,
+      backgroundColor:
+        type === 'success'
+          ? Colors.yellow2
+          : type === 'verified'
+          ? Colors.secondarySolitude
+          : Colors.secondaryBrinkPink,
+    },
+  });
 
-    if (notificationState === 'open') {
-        return (
-            <PaddingView paddingSize={2} style={styles.container}>
-                {type === 'success' ? <CircleTick /> : <Warning />}
-                {message}
-                <TouchableOpacity onPress={() => closeNotification()}>
-                    {type === 'success' ? <CloseDark />: <CloseLight />}
-                </TouchableOpacity>
-            </PaddingView>
-        )
-    }
+  if (notificationState === 'open') {
+    return (
+      <PaddingView paddingSize={2} style={styles.container}>
+        {type === 'success' ? (
+          <CircleTick />
+        ) : type === 'verified' ? (
+          <Verified width={normalize(24)} height={normalize(24)} />
+        ) : (
+          <Warning />
+        )}
+        {message}
+        <TouchableOpacity onPress={() => closeNotification()}>
+          {type === 'success' ? (
+            <CloseDark />
+          ) : type === 'verified' ? (
+            <CloseDark />
+          ) : (
+            <CloseLight />
+          )}
+        </TouchableOpacity>
+      </PaddingView>
+    );
+  }
 
-    return <></>;
-}
+  return <></>;
+};
 
 export default Notification;
