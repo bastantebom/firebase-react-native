7de178055b602f040ff4b77bf800ddd510450ece
Paul Prieto <paul@360pixels.co>
2020-08-06 13:47:04 +0800

    fix: make post popup buttons show.

--- src/context/index.js
+++ src/context/index.js
@@ -6,6 +6,7 @@ export const ContextProvider = ({ children }) => {
   const [sliderState, setSliderState] = useState('');
   const [notificationState, setNotificationState] = useState('');
   const [authType, setAuthType] = useState('');
+  const [showButtons, setShowButtons] = useState();
 
   const closeSlider = () => {
     setSliderState("close")
@@ -23,8 +24,16 @@ export const ContextProvider = ({ children }) => {
     setNotificationState('close');
   }
 
+  const openPostButtons = () => {
+    setShowButtons(true);
+  }
+
+  const closePostButtons = () => {
+    setShowButtons(false);
+  }
+
   return (
-    <Context.Provider value={{ sliderState, notificationState, closeSlider, openSlider, openNotification, closeNotification, authType, setAuthType }} >
+    <Context.Provider value={{ sliderState, notificationState, closeSlider, openSlider, openNotification, closeNotification, authType, setAuthType, openPostButtons, closePostButtons, showButtons, setShowButtons }} >
       {children}
     </Context.Provider>
   )
