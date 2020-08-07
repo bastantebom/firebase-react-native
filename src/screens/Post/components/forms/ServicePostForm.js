9b81fd3a66e1dc9498e42809fe7807897174ecb8
Paul Prieto <paul@360pixels.co>
2020-08-07 16:28:13 +0800

    chore: create function to disable post when the form is not complete.

--- src/screens/Post/components/forms/ServicePostForm.js
+++ src/screens/Post/components/forms/ServicePostForm.js
@@ -1,14 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, TouchableOpacity, ScrollView, SafeAreaView} from 'react-native';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import {Switch} from 'react-native-switch';
+import Textarea from 'react-native-textarea';
 
 import {AppText, AppInput} from '@/components';
 import {normalize, Colors} from '@/globals';
 import {PostImages} from '@/assets/images/icons';
-import Textarea from 'react-native-textarea';
 
 const ServicePostForm = () => {
+  const [buttonEnabled, setButtonEnabled] = useState(false);
   const [photoCount, setPhotoCount] = useState(0);
 
   const togglePickupState = () => {
@@ -37,11 +38,36 @@ const ServicePostForm = () => {
     setPaymentMethod('');
   };
 
+  const checkFormContent = () => {
+    if (
+      title &&
+      price &&
+      (pickupState || deliveryState) &&
+      storeLocation &&
+      paymentMethod
+    )
+      return setButtonEnabled(false);
+
+    return setButtonEnabled(true);
+  };
+
+  useEffect(() => {
+    checkFormContent();
+  }, [
+    title,
+    price,
+    pickupState,
+    deliveryState,
+    storeLocation,
+    paymentMethod,
+    description,
+  ]);
+
   return (
     <KeyboardAwareScrollView
       style={{
         flex: 1,
-        backgroundColor: Colors.neutralGray,
+        backgroundColor: Colors.neutralsZircon,
         width: normalize(375),
       }}
       extraScrollHeight={25}
@@ -87,8 +113,15 @@ const ServicePostForm = () => {
           customStyle={{marginBottom: 16}}
           label="Title"
           placeholder="Eg. Iphone, Macbook"
+          value={title}
+          onChangeText={(text) => setTitle(text)}
         />
-        <AppInput customStyle={{marginBottom: 16}} label="Price" />
+        <AppInput
+          customStyle={{marginBottom: 16}}
+          label="Price"
+          value={price}
+          onChangeText={(text) => setPrice(text)}
+        />
 
         {/* <AppInput
         // wont work because of fixed height
@@ -107,6 +140,8 @@ const ServicePostForm = () => {
             paddingHorizontal: 16,
             paddingVertical: 8,
           }}
+          defaultValue={description}
+          onChangeText={(text) => setDescription(text)}
           style={{
             color: Colors.contentEbony,
             fontFamily: 'RoundedMplus1c-Regular',
@@ -138,10 +173,9 @@ const ServicePostForm = () => {
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'space-between',
+            marginBottom: 16,
           }}>
-          <AppText textStyle="body2" customStyle={{marginBottom: 16}}>
-            Pickup
-          </AppText>
+          <AppText textStyle="body2">Pickup</AppText>
           <Switch
             value={pickupState}
             onValueChange={togglePickupState}
@@ -157,16 +191,19 @@ const ServicePostForm = () => {
             switchBorderRadius={12}
           />
         </View>
-        <AppInput label="Store Location" customStyle={{marginBottom: 16}} />
+        <AppInput
+          label="Store Location"
+          customStyle={{marginBottom: 16}}
+          value={storeLocation}
+          onChangeText={(text) => setStoreLocation(text)}
+        />
         <View
           style={{
             flexDirection: 'row',
             alignItems: 'center',
             justifyContent: 'space-between',
           }}>
-          <AppText textStyle="body2" customStyle={{marginBottom: 16}}>
-            Offer Delivery
-          </AppText>
+          <AppText textStyle="body2">Offer Delivery</AppText>
           <Switch
             value={deliveryState}
             onValueChange={toggleDeliveryState}
@@ -199,13 +236,20 @@ const ServicePostForm = () => {
           label="Payment Method"
           placeholder="Eg: Cash, Gcash"
           customStyle={{marginBottom: 64}}
+          value={paymentMethod}
+          onChangeText={(text) => setPaymentMethod(text)}
         />
 
         <TouchableOpacity
+          onPress={() => {
+            alert('title: ' + title + ' price: ' + price + ' description: ' + description);
+          }}
           activeOpacity={0.7}
-          disabled={true}
+          disabled={buttonEnabled}
           style={{
-            backgroundColor: Colors.neutralsGainsboro,
+            backgroundColor: buttonEnabled
+              ? Colors.neutralsGainsboro
+              : Colors.primaryYellow,
             paddingVertical: 12,
             alignItems: 'center',
           }}>
