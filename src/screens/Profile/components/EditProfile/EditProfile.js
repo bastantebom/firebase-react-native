1e84a8adf6e8705b9df92eed0e595d9b45286990
geraldine <geraldine@fullstack.ph>
2020-08-28 11:04:38 +0800

    fix input debounce

--- src/screens/Profile/components/EditProfile/EditProfile.js
+++ src/screens/Profile/components/EditProfile/EditProfile.js
@@ -558,7 +558,6 @@ const EditProfile = ({toggleEditProfile, toggleMenu, triggerNotify}) => {
                 label="Name"
                 customStyle={{marginBottom: normalize(16)}}
                 validation={['email', '']}
-                // validation={['email']}
               />
               <View style={{position: 'relative'}}>
                 <TouchableOpacity onPress={() => toggleMap()}>
