3f7a0ad8613e5887b09c73a90c9de6bba6e3d94b
Paul Prieto <paul@360pixels.co>
2020-08-28 14:52:21 +0800

    fix: overwrite source prop

--- src/components/ImageCache/CacheableImage.js
+++ src/components/ImageCache/CacheableImage.js
@@ -3,19 +3,18 @@ import FastImage from 'react-native-fast-image';
 
 const CacheableImage = (props) => {
   console.log('PROPS');
-  console.log(props);
+  console.log(props.source.uri);
 
   return (
     <FastImage
       // style={{width: 200, height: 200}}
-      source={{
-        uri:
-          'https://firebasestorage.googleapis.com/v0/b/codes-servbees.appspot.com/o/JlW54zJC8EVqLhxeLsP7H0dvUuT2%2Fpost-photo%2FIMG_0005?alt=media&token=f3aa3382-2be0-4693-aefc-22e665dd7436',
-        // headers: {Authorization: 'someAuthToken'},
-        priority: FastImage.priority.normal,
-      }}
       resizeMode={FastImage.resizeMode.cover}
       {...props}
+      // overwrite source prop.
+      source={{
+        uri: props?.source?.uri,
+        priority: FastImage.priority.normal,
+      }}
     />
   );
 };
