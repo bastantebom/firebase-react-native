e8ce4f09e908fa7ec5844265a056b5f18ebf1110
Jayson Ilagan <jayson@360pixels.co>
2020-09-07 13:38:47 +0800

    environment switching

--- firebase_env/prod-env.sh
+++ firebase_env/prod-env.sh
@@ -1,3 +1,4 @@
 echo “Switching to Firebase prod environment”
 yes | cp -rf prod/google-services.json ../android/app
 yes | cp -rf prod/GoogleService-Info.plist ../ios
+yes | cp -rf prod/Info.plist ../ios/servbees
