b3aef75fc5e824ce96dd73517810681c90793f54
Paul Prieto <paul@360pixels.co>
2020-09-11 14:10:41 +0800

    fix: guest post screen modal for signup and login

--- src/screens/Post/components/GuestPost.js
+++ src/screens/Post/components/GuestPost.js
@@ -1,13 +1,20 @@
 import React, {useRef, createRef, useState, useEffect, useContext} from 'react';
-import {View, SafeAreaView, StyleSheet, Dimensions} from 'react-native';
-import {AppText, AppButton, PaddingView} from '@/components';
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import Modal from 'react-native-modal';
+
+import {AppText, AppButton, PaddingView, BottomSheetHeader} from '@/components';
 import {
   OnboardingIllustration4,
   OnboardingIllustration3,
 } from '@/assets/images';
 import SignUp from '@/screens/Authentication/SignUp/SignUp';
 import Login from '@/screens/Authentication/Login/login';
-import BottomSheet from 'reanimated-bottom-sheet';
 
 import {Colors} from '@/globals';
 import {Context} from '@/context';
@@ -15,7 +22,6 @@ import {ScrollView} from 'react-native-gesture-handler';
 
 const {height, width} = Dimensions.get('window');
 
-let bottomSheetRef = createRef();
 
 export const GuestPost = () => {
   const {
@@ -24,78 +30,33 @@ export const GuestPost = () => {
     openSlider,
     authType,
     setAuthType,
+    authenticationSheet,
+    showAuthenticationSheet,
   } = useContext(Context);
 
-  useEffect(() => {
-    if (authType === '') {
-      return bottomSheetRef?.current.snapTo(1);
-    }
-
-    bottomSheetRef?.current.snapTo(1);
-
-    setTimeout(() => {
-      bottomSheetRef?.current.snapTo(0);
-    }, 1200);
-  }, [authType, setAuthType]);
-
-  const renderHeader = () => {
-    return (
-      <View
-        style={{
-          backgroundColor: 'white',
-          borderTopLeftRadius: 10,
-          borderTopRightRadius: 10,
-          alignItems: 'center',
-        }}>
-        <View
-          style={{
-            backgroundColor: '#EAEAEA',
-            width: 40,
-            height: 5,
-            marginVertical: 8,
-          }}
-        />
-      </View>
-    );
-  };
-
-  const renderContent = () => {
+  const RenderContent = () => {
     if (authType === 'signup') {
-      return (
-        <View
-          style={{
-            backgroundColor: 'white',
-            height: '100%',
-            position: 'relative',
-            zIndex: 9999,
-          }}>
-          <SignUp />
-        </View>
-      );
+      return <SignUp />;
     }
     if (authType === 'login') {
-      return (
-        <View style={{backgroundColor: 'white', height: '100%'}}>
-          <Login />
-        </View>
-      );
+      return <Login />;
     }
   };
 
-  if (sliderState === 'close') {
-    bottomSheetRef?.current.snapTo(1);
-  }
+  // if (sliderState === 'close') {
+  //   bottomSheetRef?.current.snapTo(1);
+  // }
 
   const clickHandler = () => {
     openSlider();
     // _panel.current.show();
-    bottomSheetRef?.current.snapTo(0);
+    // bottomSheetRef?.current.snapTo(0);
   };
 
   const closeHandler = () => {
     openSlider();
     // _panel.current.hide();
-    bottomSheetRef?.current.snapTo(1);
+    // bottomSheetRef?.current.snapTo(1);
   };
 
   return (
@@ -123,21 +84,37 @@ export const GuestPost = () => {
                 onPress={() => {
                   clickHandler();
                   setAuthType('signup');
-                  bottomSheetRef.current.snapTo(0);
-                  // console.log('hasjjhjh');
                 }}
               />
             </View>
           </PaddingView>
         </ScrollView>
       </SafeAreaView>
-      <BottomSheet
-        ref={bottomSheetRef}
-        snapPoints={['85%', '0%']}
-        renderContent={renderContent}
-        renderHeader={renderHeader}
-        initialSnap={1}
-      />
+
+      <Modal
+        isVisible={authenticationSheet}
+        animationIn="slideInUp"
+        animationInTiming={450}
+        animationOut="slideOutDown"
+        animationOutTiming={450}
+        style={{margin: 0, justifyContent: 'flex-end'}}
+        customBackdrop={
+          <TouchableWithoutFeedback
+            onPress={() => showAuthenticationSheet(false)}>
+            <View style={{flex: 1, backgroundColor: 'black'}} />
+          </TouchableWithoutFeedback>
+        }>
+        <View
+          style={{
+            backgroundColor: 'white',
+            height: '82%',
+            borderTopLeftRadius: 10,
+            borderTopRightRadius: 10,
+          }}>
+          <BottomSheetHeader />
+          <RenderContent />
+        </View>
+      </Modal>
     </>
   );
 };
