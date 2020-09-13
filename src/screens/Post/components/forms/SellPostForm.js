6926f164c1f23ca2abca6cf6dae838ad6eae96a0
geraldine <geraldine@fullstack.ph>
2020-09-13 21:50:01 +0800

    updated branch

--- src/screens/Post/components/forms/SellPostForm.js
+++ src/screens/Post/components/forms/SellPostForm.js
@@ -238,20 +238,6 @@ const SellPostForm = ({navToPost, togglePostModal, formState, initialData}) => {
       return Promise.reject('Failed to upload');
     }
   };
-
-  // const uploadAllImage = () => Promise.all(
-  //   postImage.map((image) => {
-  //     return uploadImageHandler(image)
-  //       .then((res) => {
-  //         console.log('res', res);
-  //         return res;
-  //       })
-  //       .catch((err) => {
-  //         console.log(err);
-  //         return err;
-  //       });
-  //   }),
-  // );
   
   const navigateToPost = async () => {
     //console.log(postImage);
