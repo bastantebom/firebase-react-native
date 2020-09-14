4521d52176d7968ede769cdca47d9084460d7df2
Paul Prieto <paul@360pixels.co>
2020-09-14 18:46:38 +0800

    fix: adjust font size +2 pts

--- src/components/AppText/AppText.js
+++ src/components/AppText/AppText.js
@@ -24,51 +24,54 @@ const AppText = ({children, textStyle, customStyle, color}) => {
   return <Text style={computedTextStyle}>{children}</Text>;
 };
 
+// first value is 14
+const defaultFontSize = 16;
+
 const styles = StyleSheet.create({
   body1: {
     fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(16),
+    fontSize: normalize(defaultFontSize+2),
     letterSpacing: 0.5,
   },
   body1medium: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(16),
+    fontSize: normalize(defaultFontSize +2),
     letterSpacing: 0.5,
   },
   body2: {
     fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(14),
+    fontSize: normalize(defaultFontSize),
     letterSpacing: 0.25,
   },
   body3: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(14),
+    fontSize: normalize(defaultFontSize),
     letterSpacing: 0.25,
   },
   button1: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(14),
+    fontSize: normalize(defaultFontSize),
     letterSpacing: 1.25,
     textTransform: 'uppercase',
   },
   button2: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(14),
+    fontSize: normalize(defaultFontSize),
     letterSpacing: 1.25,
   },
   button3: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(12),
+    fontSize: normalize(defaultFontSize-2),
     letterSpacing: 1.25,
   },
   caption: {
     fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(12),
+    fontSize: normalize(defaultFontSize-2),
     letterSpacing: 0.4,
   },
   caption2: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(12),
+    fontSize: normalize(defaultFontSize-2),
     letterSpacing: 0.4,
   },
   display1: {
@@ -98,49 +101,49 @@ const styles = StyleSheet.create({
   },
   display6: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(20),
+    fontSize: normalize(defaultFontSize+6),
     letterSpacing: 0.15,
   },
   eyebrow1: {
     fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(10),
+    fontSize: normalize(defaultFontSize-4),
     letterSpacing: 1.5,
     textTransform: 'uppercase',
   },
   eyebrow2: {
     fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(10),
+    fontSize: normalize(defaultFontSize-4),
     letterSpacing: 0.8,
   },
   price: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(12),
+    fontSize: normalize(defaultFontSize-2),
     lineHeight: 18,
   },
   subtitle1: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(18),
+    fontSize: normalize(defaultFontSize+4),
     letterSpacing: 0.15,
   },
   subtitle2: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(14),
+    fontSize: normalize(defaultFontSize),
     letterSpacing: 0.1,
   },
   promo: {
     fontFamily: 'RoundedMplus1c-Medium',
-    fontSize: normalize(12),
+    fontSize: normalize(defaultFontSize-2),
     letterSpacing: 0.4,
   },
   tabNavigation: {
     fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(14),
+    fontSize: normalize(defaultFontSize),
     letterSpacing: 2,
     textTransform: 'uppercase',
   },
   metadata: {
     fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(10),
+    fontSize: normalize(defaultFontSize-4),
     letterSpacing: 0.8,
   },
 });
