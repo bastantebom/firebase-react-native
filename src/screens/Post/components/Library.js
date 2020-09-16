1c0e3172ab05425e5ea6d44a17c385d4a7a26b77
Carl Dallo <carldallo1021@gmail.com>
2020-09-16 22:05:00 +0800

    WIP post images

--- src/screens/Post/components/Library.js
+++ src/screens/Post/components/Library.js
@@ -21,90 +21,22 @@ const { height, width } = Dimensions.get('window');
 
 export const Library = ({ cancel, next, data }) => {
   const {
-    setPostImage,
-    postImage,
-    setImageCount,
     imageCount,
     setImageCurrent,
     imageCurrent,
     selected,
-    setSelected,
-    recentImages,
-    coverPhoto,
-    setCoverPhoto,
     setLibImages
   } = useContext(Context);
 
   const [showFolderList, setShowFolderList] = useState(false);
-  // const [countSelect, setCountSelect] = useState(0);
-  // const [count, setCount] = useState([]);
   const [totalCount, setTotalCount] = useState(imageCount)
-  // const [selectedImageUrl, setSelectedImageUrl] = useState([])
-  const [imageSelected, setImageSelected] = useState('')
-
-  // const checkIndex = (arr) => {
-  //   const index = count.indexOf(arr);
-  //   return index;
-  // };
 
   const getSelectedImages = async (images) => {
     const imageUrl = [];
     images.forEach(image => imageUrl.push(image.uri ? image.uri : image))
     setLibImages(imageUrl)
-    // setSelectedImageUrl(imageUrl)
-
-    // const index = recentImages.findIndex(url => !imageUrl.includes(url))
-    // if (!~index) return
-    // const coverPhotoPlaceholder = coverPhoto
-    // coverPhotoPlaceholder.splice(index, 1)
-    // setCoverPhoto(coverPhotoPlaceholder)
-
-    // const jointArray = [...recentImages, ...imageUrl]
-    // const deselectedImage = Array.from(new Set(jointArray)).filter((url, i, arr) => arr.includes(url))
-
-    // console.log(deselectedImage)
-
-    
-
-    var num = images.length;
     setImageCurrent(num > 0 ? images[num - 1].uri : '');
-
-    // setSelected(images);
-
-    // setCountSelect(num)
-    
-    // const imageUrl = [];
-    // images.forEach(image => imageUrl.push(image.uri ? image.uri : image))
-    
-    // setPostImage(imageUrl);
-
-    // const currentCount = count;
-    // const index = checkIndex(imageUrl);
-    // if (index === -1) {
-    //   if (sum < 10) {
-    //     currentCount.push(imageUrl);
-    //     setCount(currentCount);
-    //   }
-    // }
   };
-
-  // const sum = imageCount + count.length
-
-  // console.log('countSelect number of items/images selected on lib', countSelect)
-  // console.log('count (data.length from postimageupload)', count)
-  // console.log('imageCount', imageCount)
-
-  // useEffect(() => {
-  //   if (data === null) {
-  //     return console.log('no data');
-  //   } 
-  //   if (data !== null) {
-  //     console.log('with data')
-  //   }
-  // }, [data])
-
-  // useEffect(() => {
-  // }, [imageCount])
   
   const toggleFolderList = () => {
     setShowFolderList(!showFolderList);
@@ -135,16 +67,16 @@ export const Library = ({ cancel, next, data }) => {
             </View>
           </TouchableOpacity> */}
           <TouchableOpacity
-            // disabled={postImage.length < 1 && true}
+            disabled={selected.length < 1 && true}
             onPress={() => {next()}}
             style={{paddingVertical: 5, paddingHorizontal: 25}}>
             <AppText
               textStyle="body3"
-              // color={
-              //   postImage.length < 1
-              //     ? Colors.buttonDisable
-              //     : Colors.contentOcean
-              // }
+              color={
+                selected.length < 1
+                  ? Colors.buttonDisable
+                  : Colors.contentOcean
+              }
             >
               Next
             </AppText>
