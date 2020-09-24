4aebe2a7e8a5bbcb79771897fc94efa9f2e96971
hazelle-fullstack <hazelle@fullstack.ph>
2020-09-24 16:40:32 +0800

    WIP: Activities tabs

--- src/screens/Activity/activity.js
+++ src/screens/Activity/activity.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   SafeAreaView,
   View,
@@ -6,33 +6,67 @@ import {
   StyleSheet
 } from 'react-native';
 
-import { AppText } from '@/components';
-import {Colors, normalize} from '@/globals';
+import { AppText, TabNavigation } from '@/components';
+import { Colors, normalize } from '@/globals';
 
 import IllustActivity from '@/assets/images/activity-img1.svg';
 
 const Activity = () => {
+  const [activity, setActivity] = useState({
+    onGoing: ['a'],
+    notifications: []
+  });
+
+  
+  const uploadTabs = [
+    {
+      key: 'ongoing',
+      title: 'ONGOING',
+      renderPage: <View><AppText>Ongoing</AppText></View>,
+    },
+    {
+      key: 'notifications',
+      title: 'NOTIFICATIONS',
+      renderPage: (
+        <View><AppText>Notifications</AppText></View>
+      ),
+    },
+  ];
+
   return (
     <SafeAreaView style={styles.contentWrapper}>
-      <IllustActivity />
-      <AppText 
-        textStyle="body1"
-        customStyle={{textAlign: 'center', marginTop: normalize(10)}}>
-        Get Active and Whatnots {"\n"} & Click Like or Whatever eh!
-      </AppText>
-      <View style={styles.descHolder}>
-        <AppText 
-          customStyle={{textAlign: 'center'}}>
-          Ang mas-tarush mong Shamcey Supsup ay nag-jembot-jembot ng eklat.
-        </AppText>
-      </View>
-      <TouchableOpacity
-        style={{ paddingVertical: 12, width: '100%', alignItems: 'center', backgroundColor: '#FFD400'}}>
-        <AppText textStyle="button2">
-        Explore Postings Near You
-        </AppText>
-      </TouchableOpacity>
-    </SafeAreaView>
+        {activity.onGoing.length == 0 && activity.notifications.length == 0 ? (
+          <>
+            <IllustActivity />
+            <AppText 
+              textStyle="body1"
+              customStyle={{textAlign: 'center', marginTop: normalize(10)}}>
+              Get Active and Whatnots {"\n"} & Click Like or Whatever eh!
+            </AppText>
+            <View style={styles.descHolder}>
+              <AppText 
+                customStyle={{textAlign: 'center'}}>
+                Ang mas-tarush mong Shamcey Supsup ay nag-jembot-jembot ng eklat.
+              </AppText>
+            </View>
+            <TouchableOpacity
+              style={{ paddingVertical: 12, width: '100%', alignItems: 'center', backgroundColor: '#FFD400'}}>
+              <AppText textStyle="button2">
+              Explore Postings Near You
+              </AppText>
+            </TouchableOpacity>
+          </>
+        ) : (
+          <>
+            <View style={{flexDirection: 'row', width: '100%', justifyContent: 'center'}}>
+              <AppText>My Activities</AppText>
+              <TouchableOpacity style={{position: 'absolute', right: 0}}><AppText color= {Colors.contentOcean}>Past</AppText></TouchableOpacity>
+            </View>
+            <TabNavigation routesList={uploadTabs} />
+          </>
+      )
+    }
+  </SafeAreaView>
   )
 }
 
---
+++ src/screens/Activity/past.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  View,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+
+import { AppText } from '@/components';
+
+const Past = () => {
+  return (
+    <SafeAreaView>
+      <AppText>Past Activities</AppText>
+    </SafeAreaView>
+  )
+}
+
+export default Past
