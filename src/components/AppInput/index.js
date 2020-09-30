2891eff62a475162e81760cfea2faaa0c37e52e1
Paul Prieto <paul@360pixels.co>
2020-09-30 14:03:54 +0800

    chore: apply new app input to login form

---
+++ src/components/AppInput/index.js
@@ -0,0 +1,6 @@
+import AppInput from './AppInput';
+import Validator from './Validator';
+import VF from './ValidationFunctions';
+import valueHandler from './ValueHandler';
+
+export {AppInput, Validator, VF, valueHandler};
