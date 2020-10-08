922817c471a28ab9769d46130483c7fad1400398
Jayson Ilagan <jayson@360pixels.co>
2020-10-08 13:49:34 +0800

    faq in settings

--- src/screens/Profile/components/HelpSupport/Faq.js
+++ src/screens/Profile/components/HelpSupport/Faq.js
@@ -15,64 +15,127 @@ import {
   EmailContactUs,
   CallContactUs,
   LocationContactUs,
+  ArrowDown,
 } from '@/assets/images/icons';
 import {normalize, Colors} from '@/globals';
 import Modal from 'react-native-modal';
 import {UserContext} from '@/context/UserContext';
 import AdminFunctionService from '@/services/Admin/AdminFunctions';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 
 // create a component
 const Faq = ({toggleFaq}) => {
+  const [currentSelected, setCurrentSelected] = useState(null);
+
+  let dummyData = [
+    {
+      topic: 'Topic or question here',
+      suggestion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      topic: 'Topic or question here 2',
+      suggestion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      topic: 'Topic or question here 3',
+      suggestion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      topic: 'Topic or question here 4',
+      suggestion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      topic: 'Topic or question here 5',
+      suggestion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      topic: 'Topic or question here 6',
+      suggestion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    },
+    {
+      topic: 'Topic or question here 7',
+      suggestion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ',
+    },
+    {
+      topic: 'Topic or question here 8',
+      suggestion: 'Lorem ipsum dolor sit amet, ',
+    },
+  ];
+
   return (
-    <>
-      <SafeAreaView style={{flex: 1}}>
-        <PaddingView paddingSize={3}>
-          <ScreenHeaderTitle
-            iconSize={16}
-            title="Frequently Asked Question"
-            close={toggleFaq}
-          />
-        </PaddingView>
-        <View style={{flex: 1}}>
-          <View style={[styles.contentWrapper]}>
-            <PaddingView paddingSize={3}></PaddingView>
-          </View>
+    <SafeAreaView style={{flex: 1}}>
+      <PaddingView paddingSize={3}>
+        <ScreenHeaderTitle
+          iconSize={16}
+          title="Frequently Asked Question"
+          close={toggleFaq}
+        />
+      </PaddingView>
+      <ScrollView style={{flex: 1}}>
+        <View style={styles.contentWrapper}>
+          {dummyData.map((faq, index) => {
+            return (
+              <TouchableOpacity
+                key={index}
+                onPress={() => {
+                  //setCurrentSelected(index);
+                  currentSelected === index
+                    ? setCurrentSelected(null)
+                    : setCurrentSelected(index);
+                }}
+                style={styles.faq}
+                activeOpacity={0.5}>
+                <View style={styles.topicContainer}>
+                  <View style={{flexDirection: 'row'}}>
+                    <AppText textStyle="body1medium">{faq.topic}</AppText>
+                    <View style={styles.iconContainer}>
+                      <ArrowDown height={24} width={24} />
+                    </View>
+                  </View>
+                  {currentSelected === index ? (
+                    <View>
+                      <View style={styles.suggestionContainer}>
+                        <AppText textStyle="body2">{faq.suggestion}</AppText>
+                      </View>
+                    </View>
+                  ) : null}
+                </View>
+              </TouchableOpacity>
+            );
+          })}
         </View>
-      </SafeAreaView>
-    </>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-  imageWrapper: {
-    marginBottom: normalize(16),
-    alignItems: 'center',
-  },
   contentWrapper: {
+    flex: 1,
+    justifyContent: 'flex-start',
     backgroundColor: Colors.neutralsWhite,
+    padding: 24,
   },
-  copyWrapper: {
+  topicContainer: {
+    paddingVertical: 16,
+    borderBottomColor: Colors.neutralGray,
+    borderBottomWidth: 1,
+  },
+  suggestionContainer: {
+    paddingVertical: 4,
+  },
+  iconContainer: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'flex-start',
-    marginBottom: normalize(24),
-    backgroundColor: Colors.neutralsWhite,
-  },
-  centerCopy: {
-    textAlign: 'left',
-    marginBottom: normalize(8),
-  },
-  input: {
-    color: Colors.contentEbony,
-    fontFamily: 'RoundedMplus1c-Regular',
-    fontSize: normalize(16),
-    letterSpacing: 0.5,
-    borderColor: Colors.neutralGray,
-    borderWidth: 1,
-    borderRadius: 4,
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    marginBottom: 16,
+    alignItems: 'flex-end',
   },
 });
 // define your styles
