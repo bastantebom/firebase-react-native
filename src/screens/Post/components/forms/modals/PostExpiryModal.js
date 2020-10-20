55cb77a83e8f69b4ebe21d08b14c1137bdfbf8b5
hazelle-fullstack <hazelle@fullstack.ph>
2020-10-20 11:24:56 +0800

    rebase sell-post-p2

--- src/screens/Post/components/forms/modals/PostExpiryModal.js
+++ src/screens/Post/components/forms/modals/PostExpiryModal.js
@@ -18,23 +18,15 @@ import { useNavigation } from '@react-navigation/native';
 import {
   AppText,
   ScreenHeaderTitle,
-<<<<<<< HEAD
   BottomSheetHeader,
   FloatingAppInput
-=======
-  BottomSheetHeader
->>>>>>> eb8b930... wip: date and time picker modals
 } from '@/components';
 import { normalize } from '@/globals';
 import { CalendarArrowLeft, CalendarArrowRight, ArrowRight, Calendar } from '@/assets/images/icons';
 
-<<<<<<< HEAD
 
 const PostExpiryModal = ({ closeModal }) => {
   const navigation = useNavigation();
-=======
-const PostExpiryModal = ({closeModal}) => {
->>>>>>> eb8b930... wip: date and time picker modals
   const [timeModal, showTimeModal] = useState(false);
   const [dateModal, showDateModal] = useState(false);
   const [selectedStartDate, setSelectedStartDate] = useState(null);
@@ -162,7 +154,6 @@ const PostExpiryModal = ({closeModal}) => {
             <BottomSheetHeader />
             <View style={{paddingTop: 30}}>
               <AppText textStyle="body3">Set Time</AppText>
-<<<<<<< HEAD
               <View style={{position: 'relative'}}>
                 <DateTimePicker
                   testID="dateTimePicker"
@@ -193,15 +184,6 @@ const PostExpiryModal = ({closeModal}) => {
                   </AppText>
                 </TouchableOpacity>
               </View>
-=======
-              <TouchableOpacity
-                style={{ marginTop: 40, paddingVertical: 12, width: '100%', alignItems: 'center', backgroundColor: '#FFD400', borderRadius: 3 }}
-                onPress={() => showDateModal(true)}>
-                <AppText textStyle="button2">
-                  Next: Set Date
-                </AppText>
-              </TouchableOpacity>
->>>>>>> eb8b930... wip: date and time picker modals
             </View>
             <Modal
               isVisible={dateModal}
@@ -212,34 +194,22 @@ const PostExpiryModal = ({closeModal}) => {
               style={{margin: 0, justifyContent: 'flex-end'}}
               customBackdrop={
                 <TouchableWithoutFeedback
-<<<<<<< HEAD
                   onPress={() => [showDateModal(false), showTimeModal(false)]}>
-=======
-                  onPress={() => showDateModal(false)}>
->>>>>>> eb8b930... wip: date and time picker modals
                   <View style={{flex: 1, backgroundColor: 'transparent'}} />
                 </TouchableWithoutFeedback>
               }>
               <View 
                 style={{
                 backgroundColor: 'white',
-<<<<<<< HEAD
                 borderTopLeftRadius: 10,
                 borderTopRightRadius: 10,
                 paddingHorizontal: 20,
                 paddingBottom: 30
-=======
-                height: '28%',
-                borderTopLeftRadius: 10,
-                borderTopRightRadius: 10,
-                paddingHorizontal: 20
->>>>>>> eb8b930... wip: date and time picker modals
               }}>
                 
               <BottomSheetHeader />
               <View style={{paddingTop: 30}}>
                 <AppText textStyle="body3">Set Date</AppText>
-<<<<<<< HEAD
                 <View style={{zIndex: 999999, paddingTop: 20}}>
                   <CalendarPicker
                     startFromMonday={true}
@@ -262,15 +232,6 @@ const PostExpiryModal = ({closeModal}) => {
                     </AppText>
                   </TouchableOpacity>
                 </View>
-=======
-                <TouchableOpacity
-                  style={{ marginTop: 40, paddingVertical: 12, width: '100%', alignItems: 'center', backgroundColor: '#FFD400', borderRadius: 3 }}
-                  onPress={() => [showDateModal(false), showTimeModal(false)]}>
-                  <AppText textStyle="button2">
-                    Save
-                  </AppText>
-                </TouchableOpacity>
->>>>>>> eb8b930... wip: date and time picker modals
               </View>
 
               <Modal
@@ -290,13 +251,9 @@ const PostExpiryModal = ({closeModal}) => {
             </View>
             </Modal>
           </View>
-<<<<<<< HEAD
         </Modal> */}
-=======
-        </Modal>
->>>>>>> eb8b930... wip: date and time picker modals
+      </View>
     </View>
-    </View >
   );
 }
 
