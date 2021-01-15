ba05afcf1b8274dcfc60266d7a2077e286cf6bd3
geraldine-fullstack <47315264+geraldine-fullstack@users.noreply.github.com>
2021-01-15 10:43:10 +0800

    policy and terms webview (#553)

--- src/screens/Profile/components/About/About.js
+++ src/screens/Profile/components/About/About.js
@@ -1,5 +1,4 @@
-//import liraries
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 import {
   View,
   Text,
@@ -7,38 +6,37 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Dimensions,
-} from 'react-native';
+} from 'react-native'
 
-import {ScreenHeaderTitle, PaddingView, AppText} from '@/components';
-import {ArrowRight} from '@/assets/images/icons';
-import {normalize} from '@/globals';
-import AboutServbees from '@/screens/Profile/components/About/AboutServbees';
-import PrivacyPolicy from '@/screens/Profile/components/About/PrivacyPolicy';
-import PaymentTerms from '@/screens/Profile/components/About/PaymentTerms';
-import TermsOfUse from '@/screens/Profile/components/About/TermsOfUse';
-import Modal from 'react-native-modal';
+import { ScreenHeaderTitle, PaddingView, AppText } from '@/components'
+import { ArrowRight } from '@/assets/images/icons'
+import { normalize } from '@/globals'
+import AboutServbees from '@/screens/Profile/components/About/AboutServbees'
+import PrivacyPolicy from '@/screens/Profile/components/About/PrivacyPolicy'
+import PaymentTerms from '@/screens/Profile/components/About/PaymentTerms'
+import TermsOfUse from '@/screens/Profile/components/About/TermsOfUse'
+import Modal from 'react-native-modal'
 
-// create a component
-const About = ({toggleAbout}) => {
-  const [servbees, setServbees] = useState(false);
-  const [privacyPolicy, setPrivacyPolicy] = useState(false);
-  const [paymentTerms, setPaymentTerms] = useState(false);
-  const [termsOfUse, setTermsOfUse] = useState(false);
+const About = ({ toggleAbout }) => {
+  const [servbees, setServbees] = useState(false)
+  const [privacyPolicy, setPrivacyPolicy] = useState(false)
+  const [paymentTerms, setPaymentTerms] = useState(false)
+  const [termsOfUse, setTermsOfUse] = useState(false)
 
   const toggleServbees = () => {
-    setServbees(!servbees);
-  };
+    setServbees(!servbees)
+  }
   const togglePrivacyPolicy = () => {
-    setPrivacyPolicy(!privacyPolicy);
-  };
+    setPrivacyPolicy(!privacyPolicy)
+  }
 
   const togglePaymentTerms = () => {
-    setPaymentTerms(!paymentTerms);
-  };
+    setPaymentTerms(!paymentTerms)
+  }
 
   const toggleTermsOfUse = () => {
-    setTermsOfUse(!termsOfUse);
-  };
+    setTermsOfUse(!termsOfUse)
+  }
 
   const copyScreens = [
     {
@@ -57,18 +55,18 @@ const About = ({toggleAbout}) => {
       id: 3,
       title: 'Terms of Use',
     },
-  ];
+  ]
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <PaddingView paddingSize={3}>
         <ScreenHeaderTitle title="About" close={toggleAbout} />
-        <View style={{marginTop: normalize(40)}}>
-          {copyScreens.map((item) => {
+        <View style={{ marginTop: normalize(40) }}>
+          {copyScreens.map(item => {
             return (
               <View key={item.id}>
                 <TouchableOpacity
-                  style={[styles.list, {marginBottom: 28}]}
+                  style={[styles.list, { marginBottom: 28 }]}
                   onPress={
                     item.id === 0
                       ? toggleServbees
@@ -86,11 +84,11 @@ const About = ({toggleAbout}) => {
                   <ArrowRight />
                 </TouchableOpacity>
               </View>
-            );
+            )
           })}
         </View>
       </PaddingView>
-      {/* About Servbees Modal */}
+
       <Modal
         isVisible={servbees}
         animationIn="slideInRight"
@@ -102,10 +100,9 @@ const About = ({toggleAbout}) => {
           backgroundColor: 'white',
           height: Dimensions.get('window').height,
         }}>
-        {/* <FilterSlider modalToggler={toggleModal} /> */}
         <AboutServbees toggleServbees={toggleServbees} />
       </Modal>
-      {/* Privacy Policy Modal */}
+
       <Modal
         isVisible={privacyPolicy}
         animationIn="slideInRight"
@@ -117,11 +114,9 @@ const About = ({toggleAbout}) => {
           backgroundColor: 'white',
           height: Dimensions.get('window').height,
         }}>
-        {/* <FilterSlider modalToggler={toggleModal} /> */}
         <PrivacyPolicy togglePrivacyPolicy={togglePrivacyPolicy} />
       </Modal>
 
-      {/* Payment Terms Modal */}
       <Modal
         isVisible={paymentTerms}
         animationIn="slideInRight"
@@ -133,11 +128,9 @@ const About = ({toggleAbout}) => {
           backgroundColor: 'white',
           height: Dimensions.get('window').height,
         }}>
-        {/* <FilterSlider modalToggler={toggleModal} /> */}
         <PaymentTerms togglePaymentTerms={togglePaymentTerms} />
       </Modal>
 
-      {/* Payment Terms Modal */}
       <Modal
         isVisible={termsOfUse}
         animationIn="slideInRight"
@@ -149,22 +142,18 @@ const About = ({toggleAbout}) => {
           backgroundColor: 'white',
           height: Dimensions.get('window').height,
         }}>
-        {/* <FilterSlider modalToggler={toggleModal} /> */}
         <TermsOfUse toggleTermsOfUse={toggleTermsOfUse} />
       </Modal>
     </SafeAreaView>
-  );
-};
+  )
+}
 
-// define your styles
 const styles = StyleSheet.create({
   list: {
     flexDirection: 'row',
-    // marginBottom: 28,
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-});
+})
 
-//make this component available to the app
-export default About;
+export default About
