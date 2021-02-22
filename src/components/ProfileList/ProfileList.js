58225e7fdb7b76b9d2b41fa552fa7d899f809eaa
Jayson <67724204+jayson360pixels@users.noreply.github.com>
2021-02-22 16:52:43 +0800

    Other user follow update copy (#669)
    * update copy on other user follower and followings list
    
    * update version number
    
    * follow following empty state

--- src/components/ProfileList/ProfileList.js
+++ src/components/ProfileList/ProfileList.js
@@ -1,49 +1,45 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native'
+import { View, TouchableOpacity, SafeAreaView } from 'react-native'
 
-import { AppText, PaddingView, AppInput, TabNavigation } from '@/components'
+import { AppText, PaddingView, TabNavigation } from '@/components'
 import { HeaderBackGray } from '@/assets/images/icons'
 import { normalize } from '@/globals'
 import Profiles from './components/Profiles'
-import ProfileInfoService from '@/services/Profile/ProfileInfo'
 import { UserContext } from '@/context/UserContext'
 import { Context } from '@/context'
+import Api from '@/services/Api'
+import TransitionIndicator from '../TransitionIndicator/TransitionIndicator'
 
 const ProfileList = ({ toggleProfileList, viewUserInfo, viewType }) => {
-  const { user, signOut, userInfo, setUserInfo } = useContext(UserContext)
-  const { refreshFollowerList, setRefreshFollowerList } = useContext(Context)
+  const { user } = useContext(UserContext)
+  const { refreshFollowerList } = useContext(Context)
   const { uid } = viewUserInfo
   const [followersList, setFollowersList] = useState([])
   const [followersCount, setFollowersCount] = useState()
   const [followingsList, setFollowingsList] = useState([])
   const [followingsCount, setFollowingsCount] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getConnectionList = async () => {
+    setIsLoading(true)
+    try {
+      const followers = await Api.getFollowers({ uid })
+      const following = await Api.getFollowing({ uid })
+      if (!followers.success || !following.success)
+        throw new Error(following.message || followers.message)
+      setFollowersList(followers.data.sort(a => (a.uid === user.uid ? -1 : 1)))
+      setFollowersCount(followers.data.length)
+      setFollowingsList(following.data.sort(a => (a.uid === user.uid ? -1 : 1)))
+      setFollowingsCount(following.data.length)
+    } catch (error) {
+      console.log(error.message)
+    }
+    setIsLoading(false)
+  }
 
   useEffect(() => {
     let mounted = true
-    if (uid && mounted) {
-      ProfileInfoService.getFollowers(uid)
-        .then(response => {
-          setFollowersList(
-            response.data.sort(a => (a.uid === user.uid ? -1 : 1))
-          )
-          setFollowersCount(response.data.length)
-        })
-        .catch(err => {
-          console.log('Err: ' + err)
-        })
-
-      ProfileInfoService.getFollowing(uid)
-        .then(response => {
-          setFollowingsList(
-            response.data.sort(a => (a.uid === user.uid ? -1 : 1))
-          )
-          setFollowingsCount(response.data.length)
-        })
-        .catch(err => {
-          console.log('Err: ' + err)
-        })
-    }
-
+    if (uid && mounted) getConnectionList()
     return () => {
       mounted = false
     }
@@ -51,17 +47,7 @@ const ProfileList = ({ toggleProfileList, viewUserInfo, viewType }) => {
 
   useEffect(() => {
     let mounted = true
-    if (refreshFollowerList && mounted) {
-      ProfileInfoService.getFollowing(uid)
-        .then(response => {
-          setFollowingsList(response.data)
-          setFollowingsCount(response.data.length)
-          setRefreshFollowerList(false)
-        })
-        .catch(err => {
-          console.log('Err: ' + err)
-        })
-    }
+    if (refreshFollowerList && mounted) getConnectionList()
     return () => {
       mounted = false
     }
@@ -79,6 +65,7 @@ const ProfileList = ({ toggleProfileList, viewUserInfo, viewType }) => {
             type="followers"
             viewType={viewType}
             followCount={followersCount}
+            viewUserInfo={viewUserInfo}
           />
         </View>
       ),
@@ -95,6 +82,7 @@ const ProfileList = ({ toggleProfileList, viewUserInfo, viewType }) => {
             type="following"
             viewType={viewType}
             followCount={followingsCount}
+            viewUserInfo={viewUserInfo}
           />
         </View>
       ),
@@ -104,6 +92,7 @@ const ProfileList = ({ toggleProfileList, viewUserInfo, viewType }) => {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <TransitionIndicator loading={isLoading} />
       <View style={{ flex: 1 }}>
         <PaddingView paddingSize={3}>
           <View
--- src/components/ProfileList/components/Profiles.js
+++ src/components/ProfileList/components/Profiles.js
@@ -1,12 +1,27 @@
 import AppText from '@/components/AppText/AppText'
-import React from 'react'
+import React, { useContext } from 'react'
 import { FlatList, View } from 'react-native'
-
+import { UserContext } from '@/context/UserContext'
+import { Context } from '@/context'
 import Profile from './Profile'
 import { FollowersEmpty, FollowingEmpty } from '@/assets/images'
-import { normalize } from '@/globals'
+import { normalize, Colors } from '@/globals'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import Api from '@/services/Api'
+import TransitionIndicator from '@/components/TransitionIndicator/TransitionIndicator'
 
-const Profiles = ({ data, type, viewType, toggleProfileList, followCount }) => {
+const Profiles = ({
+  data,
+  type,
+  viewType,
+  toggleProfileList,
+  followCount,
+  viewUserInfo,
+  isLoading,
+}) => {
+  const { user } = useContext(UserContext)
+  const { setRefreshFollowerList } = useContext(Context)
+
   const renderItem = ({ item }) => (
     <Profile
       data={item}
@@ -16,27 +31,73 @@ const Profiles = ({ data, type, viewType, toggleProfileList, followCount }) => {
     />
   )
 
+  const handleFollow = async () => {
+    try {
+      const followResponse = await Api.followUser({ uid: viewUserInfo?.uid })
+      if (followResponse.success) {
+        setRefreshFollowerList(true)
+      }
+    } catch (error) {}
+  }
+
   if (!followCount) {
     return (
       <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
         {type === 'followers' ? <FollowersEmpty /> : <FollowingEmpty />}
-        <AppText
-          textStyle="body1medium"
-          customStyle={{
-            marginTop: normalize(50),
-            marginBottom: normalize(8),
-          }}>
-          {type === 'followers'
-            ? 'Where’s your buzz?'
-            : 'You are not following anyone yet'}
-        </AppText>
-        <AppText
-          textStyle="body2"
-          customStyle={{ textAlign: 'center', maxWidth: '90%' }}>
-          {type === 'followers'
-            ? 'Post about your biz and the bees will follow.'
-            : 'Add more Buzzybees and bee-uild your Beeyanihan network! '}
-        </AppText>
+        {viewUserInfo?.uid === user?.uid ? (
+          <>
+            <AppText
+              textStyle="body1medium"
+              customStyle={{
+                marginTop: normalize(50),
+                marginBottom: normalize(8),
+              }}>
+              {type === 'followers'
+                ? 'Where’s your buzz?'
+                : 'You are not following anyone yet'}
+            </AppText>
+            <AppText
+              textStyle="body2"
+              customStyle={{ textAlign: 'center', maxWidth: '90%' }}>
+              {type === 'followers'
+                ? 'Post about your biz and the bees will follow.'
+                : 'Add more Buzzybees and bee-uild your Beeyanihan network! '}
+            </AppText>
+          </>
+        ) : (
+          <>
+            <AppText
+              textStyle="body1medium"
+              customStyle={{
+                marginTop: normalize(50),
+                marginBottom: normalize(8),
+              }}>
+              {type === 'followers'
+                ? `${viewUserInfo.full_name} has no followers yet`
+                : `${viewUserInfo.full_name} is not following anyone yet`}
+            </AppText>
+            <AppText
+              textStyle="body2"
+              customStyle={{ textAlign: 'center', maxWidth: '90%' }}>
+              {type === 'followers'
+                ? `Bee a friend today. Connect with ${viewUserInfo.full_name}`
+                : ''}
+            </AppText>
+            {type === 'followers' && (
+              <TouchableOpacity onPress={handleFollow}>
+                <AppText
+                  textStyle="body2medium"
+                  customStyle={{
+                    textAlign: 'center',
+                    color: Colors.contentOcean,
+                    marginTop: normalize(16),
+                  }}>
+                  Follow
+                </AppText>
+              </TouchableOpacity>
+            )}
+          </>
+        )}
       </View>
     )
   }
