009e7997d8f753b7c94971afd2a792eb0827e802
carl-fullstack <47442076+carl-fullstack@users.noreply.github.com>
2021-06-08 13:02:12 +0800

    fixed, login text (#945)
    * fixed, login text
    
    * fixed, comment
    
    Co-authored-by: Carl Dallo <carldallo1021@gmail.com>

--- src/screens/Verification/verify-code.js
+++ src/screens/Verification/verify-code.js
@@ -163,7 +163,9 @@ const VerifyCodeScreen = ({ navigation, route }) => {
         <View style={[styles.alignCenter, styles.spacingBottomx2]}>
           <Text style={typography.body2narrow}>
             A 4-digit code has been sent to{' '}
-            <Text style={typography.medium}>{login}</Text>
+            <Text style={typography.medium}>
+              {isEmail(login) ? login : `+63${login.slice(-10)}`}
+            </Text>
           </Text>
         </View>
         <View style={[styles.verificationWrapper, styles.spacingBottomx4]}>
